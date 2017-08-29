799290cf4f8f4384179665cc8ffbec39e9892da0
Soumik Tarafdar <soumiktarafdar@gmail.com>
2017-08-29 19:59:16 +0530

    Login and registration pages completed

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {RegisterPage} from '../register/register';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  registerPage = RegisterPage;
 
   constructor(public navCtrl: NavController) {
 
   }
 
+
+  goToRegistration(){
+    this.navCtrl.push(RegisterPage);
+  }
 }
